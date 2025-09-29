f0aa2f428fb8397b374ea3b43f6068a44e08a251
Ana Robles <anam.robles.garcia@gmail.com>
2025-09-29 22:34:55 +0200

    BBDD en proceso

--- notebooks/Fase_4_BBDD/funciones_bbdd.py
+++ notebooks/Fase_4_BBDD/funciones_bbdd.py
@@ -1,10 +1,8 @@
 #%%
 
 # importamos las bibliotecas y conectores necesarios
-
 import mysql.connector
 from mysql.connector import errorcode
-
 import pandas as pd
 
 # Importamos nuestra librería de soporte
@@ -59,6 +57,8 @@ def creacion_bbdd(cnx,query_creacion_bbdd, query_movernos_bbdd):
 
 # %%    
 
+# Aceso a BASE DE DATOS existente:
+
 def acceso_bbdd(cnx,nombre_bbdd):  # USE nombre_bbdd
     mycursor = cnx.cursor()   # creamos nuestro cursor (nos abre mysql)
     print(f"Variable 'mycursor' creada.")
@@ -72,6 +72,8 @@ def acceso_bbdd(cnx,nombre_bbdd):  # USE nombre_bbdd
 
 # %%
 
+# Creación de TABLAS
+
 def creacion_tablas(mycursor, *args): # para meter tantas queries como queramos
     try:
         for query in args:
@@ -82,22 +84,175 @@ def creacion_tablas(mycursor, *args): # para meter tantas queries como queramos
 
 # %%
 
-def insercion_tabla_departamento(mycursor, cnx, df):
+# INSERCIÓN datos: tablas generales
+
+    # Departamento
+
+def insercion_tabla_departamento(mycursor, cnx, df_trabajo):
+    # quitamos los valores duplicados
+    df = df_trabajo[['department']].drop_duplicates() # 3 entradas
+    # iteramos en el df para sacar los valores que necesitamos
     tabla_departamento=[]
     for row in df[['department']].itertuples():
         tabla_departamento.append((row.department,))
     
     try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
         mycursor.executemany(query.query_insercion_departamento, tabla_departamento)
         cnx.commit()
         print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
     except Exception as e:
         print(f"No se ha podido realizar la inserción:\n{e}")
         
+    # Puesto
+
+def insercion_tabla_puesto(mycursor, cnx, df_puesto):
+    # quitamos los valores duplicados
+    df= df_puesto[['jobrole', 'joblevel']]
+    df = df.drop_duplicates().reset_index(drop = True)  # 26 entradas
+    # iteramos en el df para sacar los valores que necesitamos
+    tabla_puesto =[]
+    for row in df[['jobrole', 'joblevel']].itertuples():
+        tabla_puesto.append((row.jobrole,row.joblevel,))
+    
+    try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
+        mycursor.executemany(query.query_insercion_puesto, tabla_puesto)
+        cnx.commit()
+        print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
+    except Exception as e:
+        print(f"No se ha podido realizar la inserción:\n{e}")
+
+    # Educación
+
+def insercion_tabla_educacion(mycursor, cnx, df_educacion):
+    # quitamos los valores duplicados
+    df = df_educacion[['education', 'educationfield']]
+    df = df.drop_duplicates().reset_index(drop = True)   # 29 entradas
+    # iteramos en el df para sacar los valores que necesitamos
+    tabla_educacion =[]
+    for row in df[['education', 'educationfield']].itertuples():
+        tabla_educacion.append((row.education,row.educationfield,))
+    
+    try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
+        mycursor.executemany(query.query_insercion_educacion, tabla_educacion)
+        cnx.commit()
+        print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
+    except Exception as e:
+        print(f"No se ha podido realizar la inserción:\n{e}")
+
+# %%
+
+# INSERCIÓN datos: tabla ppal y dependientes
+
+    # Empleados <-- tabla principal, KP y KF
+
+def insercion_tabla_empleados(mycursor, cnx, df):
+    # Obtener IDs FOREIGN KEYS de las otras tablas YA CREADAS 
+    mycursor.execute(query.query_id_tabla_departamento)
+    departamentoID = {nombre: id for id, nombre in mycursor.fetchall()} # <-- diccionario
+    
+    mycursor.execute(query.query_id_tabla_puesto)
+    puestoID = {(nombre, nivel): id for id, nombre, nivel in mycursor.fetchall()} # <-- diccionario
+    
+    mycursor.execute(query.query_id_tabla_educacion)
+    educacionID = {(nivel, campo): id for id, nivel, campo in mycursor.fetchall()} # <-- diccionario
+
+    # iteramos en el df para sacar los valores que necesitamos
+    tabla_empleados =[]
+    for row in df[['employeenumber', 'age', 'attrition', 'gender', 'maritalstatus', 'datebirth',
+                   'department', 'jobrole', 'joblevel', 'education', 'educationfield'
+                   ]].itertuples():
+        try:
+            # Buscamos el ID correspondiente a cada fila del DataFrame, para añadirlo a la tabla_empleados
+            departamento_id= departamentoID[row.department]
+            puesto_id= puestoID[(row.jobrole, row.joblevel)]
+            educacion_id = educacionID[(row.education, row.educationfield)]
+
+            tabla_empleados.append((
+                row.employeenumber,
+                row.age,
+                row.attrition,
+                row.gender,
+                row.maritalstatus,
+                row.datebirth,
+                departamento_id,
+                puesto_id,
+                educacion_id,
+                ))
+            
+        except KeyError as e:
+            print(f"Error de clave foránea no encontrada: {e}\nFila problemática: {row}")
+
+    try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
+        mycursor.executemany(query.query_insercion_empleados, tabla_empleados)
+        cnx.commit()
+        print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
+    except Exception as e:
+        print(f"No se ha podido realizar la inserción:\n{e}")
+
+
+    # Nivel_Satisfaccion <-- KF
+
+def insercion_tabla_nivel_satisfaccion(mycursor, cnx, df):
+    # iteramos en el df para sacar los valores que necesitamos
+    tabla_nivel_satisfaccion =[]
+    for row in df[['environmentsatisfaction', 'jobsatisfaction', 'relationshipsatisfaction', 'employeenumber']].itertuples():
+        tabla_nivel_satisfaccion.append((
+            row.environmentsatisfaction,
+            row.jobsatisfaction,
+            row.relationshipsatisfaction,
+            row.employeenumber,   # clave foránea (está tb en el df)
+            ))
+    
+    try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
+        mycursor.executemany(query.query_insercion_nivel_satisfaccion, tabla_nivel_satisfaccion)
+        cnx.commit()
+        print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
+    except Exception as e:
+        print(f"No se ha podido realizar la inserción:\n{e}")
+
+
+    # Condiciones_Laborales
+
+def insercion_tabla_condiciones_laborales(mycursor, cnx, df):
+    # iteramos en el df para sacar los valores que necesitamos
+    tabla_laborales =[]
+    for row in df[['businesstravel', 'distancefromhome', 'jobinvolvement', 'numcompaniesworked', 'overtime', 'performancerating', 'stockoptionlevel', 'trainingtimeslastyear', 'worklifebalance', 'yearsatcompany', 'yearssincelastpromotion', 'yearswithcurrmanager', 'salary', 'remotework', 'employeenumber']].itertuples():
+        tabla_laborales.append((
+            row.businesstravel,
+            row.distancefromhome,
+            row.jobinvolvement,
+            row.numcompaniesworked,
+            row.overtime,
+            row.performancerating,
+            row.stockoptionlevel,
+            row.trainingtimeslastyear,
+            row.worklifebalance,
+            row.yearsatcompany,
+            row.yearssincelastpromotion,
+            row.yearswithcurrmanager,
+            row.salary,
+            row.remotework,
+            row.employeenumber,   # clave foránea (está tb en el df)
+            ))
+    
+    try:
+        # Le pedimos a sql que nos ejecute lo que indicamos en el archivo de soporte
+        mycursor.executemany(query.query_insercion_condiciones_laborales, tabla_laborales)
+        cnx.commit()
+        print(f"Datos insertados correctamente: {mycursor.rowcount} filas.")
+    except Exception as e:
+        print(f"No se ha podido realizar la inserción:\n{e}")
+
+
+        
 # %%
 
 # Cerramos la conexión
-
 def cerrar_conexion(cnx):
     cnx.close()
     print("Conexión cerrada.")
--- notebooks/Fase_4_BBDD/funciones_soporte_queries.py
+++ notebooks/Fase_4_BBDD/funciones_soporte_queries.py
@@ -1,10 +1,10 @@
-# Creamos el schema / BBDD
+################## Creamos el schema / BBDD
 query_creacion_bbdd = 'CREATE SCHEMA IF NOT EXISTS `Optimizacion_Talento` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;'
 
-# Nos posicionamos en la BBDD creada
+##################  Nos posicionamos en la BBDD creada
 query_moverse_bbdd = 'USE `Optimizacion_Talento` ;'
 
-# Creación de tablas
+##################  Creación de tablas
 
     # Tabla A: Departamento
 query_creacion_tabla_departamento = '''
@@ -19,7 +19,7 @@ print("Tabla 'Departamento' creada.")
 query_creacion_tabla_puesto = '''
 CREATE TABLE IF NOT EXISTS Puesto (
   PuestoID INT AUTO_INCREMENT PRIMARY KEY,
-  NombrePuesto VARCHAR(100) NOT NULL UNIQUE,
+  NombrePuesto VARCHAR(100) NOT NULL,
   NivelPuesto INT
   );'''
 # 12  jobrole --> object 
@@ -31,30 +31,64 @@ query_creacion_tabla_educacion = '''
 CREATE TABLE IF NOT EXISTS Educacion_Nivel_Campo (
   EducacionID INT AUTO_INCREMENT PRIMARY KEY,
   Nivel INT NOT NULL, 
-  Campo VARCHAR(100) NOT NULL, 
-  UNIQUE KEY UQ_Nivel_Campo (Nivel, Campo)
+  Campo VARCHAR(100) 
   );'''
 #  5   education --> int64 
 #  6   educationfield --> object
 print("Tabla 'Educacion_Nivel_Campo' creada.")
 
-    # Tabla D: Nivel_Satisfaccion
+# --- DEFINICIÓN DE TABLA CENTRAL (Con FKs) ---
+
+# Tabla PPAL: Empleados (Tabla Central)
+query_creacion_tabla_empleados = '''
+CREATE TABLE IF NOT EXISTS Empleados (
+    EmployeeNumber INT PRIMARY KEY,
+    Edad INT NOT NULL,
+    EnEmpresa VARCHAR(3),
+    Genero VARCHAR(10),
+    EstadoCivil VARCHAR(20), 
+    AnoNacimiento YEAR, 
+
+    -- Claves Foráneas
+    DepartamentoFK INT,
+    PuestoFK INT,
+    EducacionFK INT,
+  
+      
+    -- Definición de Relaciones de Claves Foráneas
+    FOREIGN KEY (DepartamentoFK) REFERENCES Departamento(DepartamentoID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (PuestoFK) REFERENCES Puesto(PuestoID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (EducacionFK) REFERENCES Educacion_Nivel_Campo(EducacionID) ON DELETE CASCADE ON UPDATE CASCADE
+    );'''
+# 7   employeenumber -->  int64 (ya únicos en el df)
+# 0   age --> int64 
+# 1   attrition --> object   
+# 9   gender --> object 
+# 14  maritalstatus --> object 
+# 25  datebirth --> int64  
+print("Tabla 'Empleados' creada con todas las FKs.")
+
+    # Tabla E: Nivel_Satisfaccion
 query_creacion_tabla_nivel_satisfaccion = '''
 CREATE TABLE IF NOT EXISTS Nivel_Satisfaccion (
-  NSatisfaccionID INT AUTO_INCREMENT PRIMARY KEY,
   NSAmbienteLaboral  INT, 
   NSLaboral INT, 
-  NSRelInterpersonales INT
+  NSRelInterpersonales INT,
+
+  -- Claves Foráneas
+  EmployeeNumberFK INT,
+
+  -- Definición de Relaciones de Claves Foráneas
+  FOREIGN KEY (EmployeeNumberFK) REFERENCES Empleados (EmployeeNumber) ON DELETE CASCADE ON UPDATE CASCADE
   );'''
 #  8   environmentsatisfaction --> int64 
 #  13  jobsatisfaction --> int64 
 #  18  relationshipsatisfaction --> int64
 print("Tabla 'Nivel_Satisfaccion' creada.")
 
-    # Tabla E: Condiciones_Laborales
+    # Tabla F: Condiciones_Laborales
 query_creacion_tabla_condiciones_laborales = '''
 CREATE TABLE IF NOT EXISTS Condiciones_Laborales (
-  CLaboralesID INT AUTO_INCREMENT PRIMARY KEY,
   ViajesTrabajo VARCHAR(50), 
   DistanciaVivienda INT, 
   NivelImplicacion INT,
@@ -68,7 +102,14 @@ CREATE TABLE IF NOT EXISTS Condiciones_Laborales (
   AñosDesdePromocion INT,
   AñosMismoManager INT,
   Salario DECIMAL(10, 2),
-  Teletrabajo VARCHAR(3)
+  Teletrabajo VARCHAR(3),
+
+  -- Claves Foráneas
+  EmployeeNumberFK INT,
+
+  -- Definición de Relaciones de Claves Foráneas
+  FOREIGN KEY (EmployeeNumberFK) REFERENCES Empleados (EmployeeNumber) ON DELETE CASCADE ON UPDATE CASCADE
+
   );'''
 # 2   businesstravel --> object 
 # 4   distancefromhome -->  int64   
@@ -86,32 +127,36 @@ CREATE TABLE IF NOT EXISTS Condiciones_Laborales (
 # 27  remotework -->  object
 print("Tabla 'Condiciones_Laborales' creada.")
 
-# --- DEFINICIÓN DE TABLA CENTRAL (Con FKs) ---
+##################  Inserción de Datos
 
-# Tabla F: Empleados (Tabla Central)
-query_creacion_tabla_empleados = '''
-CREATE TABLE IF NOT EXISTS Empleados (
-    EmployeeNumber INT PRIMARY KEY,
-    Edad INT NOT NULL,
-    EnEmpresa VARCHAR(3),
-    Genero VARCHAR(10),
-    EstadoCivil VARCHAR(20), 
-    AñoNacimiento YEAR, 
+    # Departamento
+query_insercion_departamento = '''
+INSERT IGNORE INTO departamento (NombreDepartamento)
+VALUES (%s);
+'''
+print("Datos insertados en tabla 'Departamento'.")
 
-    -- Claves Foráneas
-    DepartamentoFK INT,
-    PuestoFK INT,
-    EducacionFK INT,
-    NSatisfaccionFK INT,
-    CLaboralesFK INT,
-      
-    -- Definición de Relaciones de Claves Foráneas
-    FOREIGN KEY (DepartamentoFK) REFERENCES Departamento (DepartamentoID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (PuestoFK) REFERENCES Puesto(PuestoID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (EducacionFK) REFERENCES Educacion_Nivel_Campo(EducacionID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (NSatisfaccionFK) REFERENCES Nivel_Satisfaccion(NSatisfaccionID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (CLaboralesFK) REFERENCES Condiciones_Laborales(CLaboralesID) ON DELETE CASCADE ON UPDATE CASCADE
-    );'''
+    # Puesto
+query_insercion_puesto = '''
+INSERT IGNORE INTO puesto (NombrePuesto, NivelPuesto)
+VALUES (%s,%s);
+'''
+print("Datos insertados en tabla 'Puesto'.")
+
+    # Educación
+query_insercion_educacion = '''
+INSERT IGNORE INTO educacion_nivel_campo (Nivel, Campo)
+VALUES (%s,%s);
+'''
+print("Datos insertados en tabla 'Educación'.")
+
+#####  Tabla ppal y dependientes
+
+    # Empleados
+query_insercion_empleados = '''
+INSERT IGNORE INTO empleados (EmployeeNumber, Edad, EnEmpresa, Genero, EstadoCivil, AnoNacimiento, DepartamentoFK, PuestoFK, EducacionFK)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
+   '''
 # 7   employeenumber -->  int64 (ya únicos en el df)
 # 0   age --> int64 
 # 1   attrition --> object   
@@ -120,11 +165,31 @@ CREATE TABLE IF NOT EXISTS Empleados (
 # 25  datebirth --> int64  
 print("Tabla 'Empleados' creada con todas las FKs.")
 
-# Inserción de Datos
-    # Departamento
-query_insercion_departamento = '''
-INSERT IGNORE INTO departamento (NombreDepartamento)
-VALUES (%s);
+query_id_tabla_departamento = '''
+SELECT DepartamentoID, NombreDepartamento 
+FROM Departamento;
 '''
-#  3   department --> object 
-print("Datos insertado en tabla 'Departamento'.")
+
+query_id_tabla_puesto = '''
+SELECT PuestoID, NombrePuesto, NivelPuesto 
+FROM Puesto;
+'''
+
+query_id_tabla_educacion = '''
+SELECT EducacionID, Nivel, Campo
+FROM Educacion_Nivel_Campo;
+'''
+
+    # Nivel_Satisfaccion
+query_insercion_nivel_satisfaccion  = '''
+INSERT IGNORE INTO nivel_satisfaccion  (NSAmbienteLaboral, NSLaboral, NSRelInterpersonales, EmployeeNumberFK)
+VALUES (%s,%s,%s,%s);
+'''
+print("Datos insertados en tabla 'Nivel_Satisfaccion'.")
+
+    # Condiciones_Laborales
+query_insercion_condiciones_laborales = '''
+INSERT IGNORE INTO condiciones_laborales (ViajesTrabajo, DistanciaVivienda, NivelImplicacion, NumEmpresasAnteriores, HorasExtra, Rendimiento, OpcionesAcciones, HorasFormacionAñoPasado, NivelConciliacion, AñosEmpresa, AñosDesdePromocion, AñosMismoManager, Salario, Teletrabajo, EmployeeNumberFK )
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+'''
+print("Datos insertados en tabla 'Condiciones_Laborales'.")
